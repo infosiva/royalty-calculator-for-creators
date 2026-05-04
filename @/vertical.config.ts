bf1dd9ff9ed1d748299ed4b942531ebba4c60f8f
sivaprakasam sivaprakasam <info.siva@gmail.com>
2026-05-04 13:36:02 +0000

    fix(health): Ensure that the config file exists and is properly exported. [business-agent]

---
+++ @/vertical.config.ts
@@ -0,0 +1,9 @@
+export default {
+  "metaTitle": "Royalty Calculator",
+  "metaDescription": "Calculate royalties for creators",
+  "keywords": "royalty calculator, creator tools",
+  "themeColor": "violet",
+  "name": "Royalty Calculator",
+  "subjects": [],
+  "isAiTool": () => false
+}
